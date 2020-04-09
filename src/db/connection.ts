dd82b36cefc0c8d813e33000640f871a79261a5d
Dalin  Oluoch <dc_dalin@Some-Network.local>
2020-04-09 22:56:29 +0300

    Deploy

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize-typescript";
 import accessEnv from "../helpers/accessEnv";
 import models from "../models";
 
-const DB_URL = accessEnv("DB_URL");
+const DATABASE_URL = accessEnv("DATABASE_URL");
 
-const sequelize = new Sequelize(DB_URL, {
+const sequelize = new Sequelize(DATABASE_URL, {
 	dialectOptions: {
 		charset: "utf8",
 		multipleStatements: true,
