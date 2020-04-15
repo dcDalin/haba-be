e39f7fde755daf71aa111b43437bdb1445e0ba99
Dalin  Oluoch <dc_dalin@Some-Network.local>
2020-04-15 08:28:26 +0300

    Change mpesa lib

--- src/utils/mpesa.ts
+++ src/utils/mpesa.ts
@@ -1,18 +1,30 @@
-import { Mpesa } from 'mpesa-api';
+// @ts-ignore
+import Mpesa from 'mpesa-node';
 import accessEnv from '../helpers/accessEnv';
 import path from 'path';
 const CONSUMER_KEY = accessEnv('CONSUMER_KEY');
 const CONSUMER_SECRET = accessEnv('CONSUMER_SECRET');
-const INITIATOR_PASSWORD = accessEnv('INITIATOR_PASSWORD');
+const INITIATOR_NAME = accessEnv('INITIATOR_NAME');
+const SECURITY_CREDENTIAL = accessEnv('SECURITY_CREDENTIAL');
+const LIPA_NA_MPESA_ONLINE_SHORT_CODE = accessEnv(
+  'LIPA_NA_MPESA_ONLINE_SHORT_CODE'
+);
 const MPESA_ENVIRONMENT = accessEnv('MPESA_ENVIRONMENT');
+const SHORT_CODE = accessEnv('SHORT_CODE');
+const LIPA_NA_MPESA_ONLINE_PASSKEY = accessEnv('LIPA_NA_MPESA_ONLINE_PASSKEY');
 
 const credentials = {
-  client_key: CONSUMER_KEY,
-  client_secret: CONSUMER_SECRET,
-  initiator_password: INITIATOR_PASSWORD,
-  certificatepath: path.resolve('src/keys/cert.cer'),
+  consumerKey: CONSUMER_KEY,
+  consumerSecret: CONSUMER_SECRET,
+  environment: MPESA_ENVIRONMENT,
+  shortCode: SHORT_CODE,
+  initiatorName: INITIATOR_NAME,
+  lipaNaMpesaShortCode: LIPA_NA_MPESA_ONLINE_SHORT_CODE,
+  lipaNaMpesaShortPass: LIPA_NA_MPESA_ONLINE_PASSKEY,
+  securityCredential: SECURITY_CREDENTIAL,
+  certPath: path.resolve('src/keys/cert.cer'),
 };
 
-const mpesa = new Mpesa(credentials, MPESA_ENVIRONMENT);
+const mpesa = new Mpesa(credentials);
 
 export default mpesa;
